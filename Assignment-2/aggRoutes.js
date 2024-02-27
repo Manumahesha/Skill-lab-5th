68c89bee0c63c591770263b6afeda2ab533cff43
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2024-02-27 20:33:03 +0530

    Add files via upload

---
+++ Assignment-2/aggRoutes.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const router = express.Router();
+const blogController = require('../controllers/aggController');
+const authenticationMiddleware = require('../middleWares/authenticationMiddleware');
+
+
+// router.get('/registered-users', blogController.getBlogsForRegisteredUsers);
+router.get('/subscribed-users',authenticationMiddleware, blogController.getBlogsForUsers);
+router.get('/reviews-rating', blogController.getReviewsAndRating);
+router.get('/date-range', blogController.getBlogsInDateRange);
+router.get('/category/:category', blogController.getBlogsByCategory);
+
+module.exports = router;
